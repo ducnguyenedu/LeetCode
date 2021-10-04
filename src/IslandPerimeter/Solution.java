0ce72a424d2e65e8f70c3342b7a9588813b38175
Nguyen Minh Duc <86579678+ducnguyenedu@users.noreply.github.com>
2021-10-04 15:20:18 +0700

    commit

---
+++ src/IslandPerimeter/Solution.java
@@ -0,0 +1,33 @@
+package IslandPerimeter;
+
+import java.io.IOException;
+
+public class Solution {
+    public static int islandPerimeter(int[][] grid) {
+        if (grid == null ||
+                grid.length == 0)
+            return 0;
+        int perimeter = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    if (i > 0 &&
+                            grid[i - 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                    if (j > 0 &&
+                            grid[i][j - 1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+    public static void main(String[] args) throws IOException {
+        int[][] grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
+        islandPerimeter(grid);
+
+    }
+}
