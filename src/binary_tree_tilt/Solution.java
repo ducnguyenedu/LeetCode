305076a05bad423c7e51d34826dce5d6898ab4d4
Nguyen Minh Duc <86579678+ducnguyenedu@users.noreply.github.com>
2021-12-08 20:27:17 +0700

    commit

---
+++ src/binary_tree_tilt/Solution.java
@@ -0,0 +1,58 @@
+package binary_tree_tilt;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+public class Solution {
+    int ans;
+
+    public int findTilt(TreeNode root) {
+        postOrder(root);
+        return ans;
+
+    }
+
+    int postOrder(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = postOrder(root.left);
+        int right = postOrder(root.right);
+
+        ans += Math.abs(left - right);
+
+        return root.val + left + right;
+
+
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
