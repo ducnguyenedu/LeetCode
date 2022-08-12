5f01ab8cb275554d298b18017b975bf805d32496
hideongloomist <108848660+hideongloomist@users.noreply.github.com>
2022-08-12 08:13:56 +0700

    https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/

---
+++ kotlin/lowest-common-ancestor-of-a-binary-search-tree/Solution.kt
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int = 0) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+class Solution {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        if (root == null) return null
+
+        val valP = p?.`val` ?: -1
+        val valQ = q?.`val` ?: -1
+
+        return when {
+            valP > root.`val` && valQ > root.`val` ->
+                lowestCommonAncestor(root.right, p, q)
+
+            valP < root.`val` && valQ < root.`val` ->
+                lowestCommonAncestor(root.left, p, q)
+
+            else -> root
+        }
+    }
+}
+
+class TreeNode(var `val`: Int = 0) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
