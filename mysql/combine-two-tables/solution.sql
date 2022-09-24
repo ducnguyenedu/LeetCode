1689a5bd2d96349033a6f63c70797025a7ab760e
Nguyễn Minh Đức <108848660+hideongloomist@users.noreply.github.com>
2022-09-24 14:43:37 +0700

    Update solution.sql

--- mysql/combine-two-tables/solution.sql
+++ mysql/combine-two-tables/solution.sql
@@ -1,12 +1,11 @@
-# Write your MySQL query statement below
-Select
-    p.firstname,
-    p.lastname,
-    a.city,
-    a.state
-from 
-    person p
-LEFT JOIN
-    Address a
-on
-    p.personId = a.personId;
+#
+Write your MySQL query statement below
+Select p.firstname,
+       p.lastname,
+       a.city,
+       a.state
+from person p
+         LEFT JOIN
+     Address a
+     on
+         p.personId = a.personId;
