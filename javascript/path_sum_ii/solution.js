f5822fd87bde1e35d33e5595757639a4b29e1567
Nguyễn Minh Đức <108848660+hideongloomist@users.noreply.github.com>
2022-09-24 14:43:28 +0700

    https://leetcode.com/problems/path-sum-ii/

---
+++ javascript/path_sum_ii/solution.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function (root, targetSum) {
+    const result = [];
+
+    function traverse(node, arr, sum) {
+        if (!node) {
+            return null
+        }
+        arr.push(node.val);
+        sum += node.val;
+        if (node.left == null && node.right == null) {
+            if (targetSum === sum) {
+                result.push([...arr]);
+                return;
+            }
+        }
+        if (traverse(node.left, arr, sum) !== null) {
+            arr.pop()
+        }
+        if (traverse(node.right, arr, sum) !== null) {
+            arr.pop();
+        }
+    }
+
+    traverse(root, [], 0)
+    return result;
+};
