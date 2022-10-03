9054c30a7d9ef4ca9872afdca57f69beeb357c08
Nguyễn Minh Đức <108848660+hideongloomist@users.noreply.github.com>
2022-10-03 21:00:41 +0700

    https://leetcode.com/problems/minimum-time-to-make-rope-colorful/

---
+++ c++/minimum_time_to_make_rope_colorful/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime)
+    {
+        int ans = 0, maxNumber = 0, currentSum = 0;
+
+        char prev = '0';
+
+        for(int i=0; i<colors.size(); i++)
+        {
+            if(prev == colors[i])
+            {
+                currentSum += neededTime[i];
+                maxNumber = max(maxNumber, neededTime[i]);
+            }
+            else
+            {
+                ans += currentSum - maxNumber;
+
+                maxNumber =  neededTime[i];
+                currentSum = neededTime[i];
+            }
+
+            prev = colors[i];
+        }
+
+        ans += currentSum - maxNumber;
+
+        return ans;
+    }
+};
