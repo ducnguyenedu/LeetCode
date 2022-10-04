503b428572bc4667cde063002582176e473aa2de
Nguyễn Minh Đức <108848660+hideongloomist@users.noreply.github.com>
2022-10-04 20:11:36 +0700

    format code

--- c++/minimum_time_to_make_rope_colorful/solution.cpp
+++ c++/minimum_time_to_make_rope_colorful/solution.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    int minCost(string colors, vector<int>& neededTime)
-    {
+    int minCost(string colors, vector<int> &neededTime) {
         int ans = 0, maxNumber = 0, currentSum = 0;
 
         char prev = '0';
 
-        for(int i=0; i<colors.size(); i++)
-        {
-            if(prev == colors[i])
-            {
+        for (int i = 0; i < colors.size(); i++) {
+            if (prev == colors[i]) {
                 currentSum += neededTime[i];
                 maxNumber = max(maxNumber, neededTime[i]);
-            }
-            else
-            {
+            } else {
                 ans += currentSum - maxNumber;
 
-                maxNumber =  neededTime[i];
+                maxNumber = neededTime[i];
                 currentSum = neededTime[i];
             }
 
