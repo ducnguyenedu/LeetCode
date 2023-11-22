aaab5a601f0ce4f1187a20a183012a7bc9cb1955
Nguyễn Minh Đức <hideongloomist@gmail.com>
2023-11-22 10:49:46 +0700

    https://leetcode.com/problems/diagonal-traverse-ii/

---
+++ javascript/diagonal_traverse_ii/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+var findDiagonalOrder = function(nums) {
+    let vals = []; 
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums[i].length; j++) {
+            vals.push([i, i + j, nums[i][j]]);
+        }
+    }
+    vals.sort((a, b) => {
+        if (a[1] === b[1]) return b[0] - a[0];
+        return a[1] - b[1];
+    })
+    return vals.map(val => val[2]);
+};
